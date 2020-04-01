5db3420c24021a72e6a92c4f9fc8f7f32ede143a
oliver-tarrago <olivereinard.tarrago@e-campus.uab.cat>
2020-04-01 18:14:42 +0200

    Pr5Ex513 refactoritzat a partir del Pr5Ex512

--- Pr5Ex513.c
+++ Pr5Ex513.c
@@ -9,66 +9,92 @@
  */
 
 
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
-
-#define NAR 7
-#define MAXARNO 5
 
 typedef struct {
     unsigned n1, n2, estat;
 } aresta;
 
 typedef struct {
-    unsigned nodes[NAR + 1];
-    unsigned arvis[NAR];
+    unsigned *nodes;
+    unsigned *arvis;
     int naresvisit;
 } estructuracami;
 
+unsigned llegir_arestes(FILE *fitxer, aresta larestes[], unsigned num_arestes);
+unsigned max_arestes(unsigned num_nodes, aresta larestes[], unsigned num_arestes);
+unsigned int calcular_valencies(aresta *larestes, unsigned num_arestes, unsigned *valencies, unsigned num_nodes);
 unsigned arestesdelnode(aresta larestes[], unsigned num_arestes, unsigned nn, unsigned int llistap[]);
 
-int calculavalencies(unsigned, unsigned[]);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Ús: Pr5Ex512 fitxer.txt\nfitxer.txt ha de contenir:\n"
+               "A la primera línia el número de nodes\n"
+               "A la segona línia el número d'arestes\n"
+               "A les següents línies, les arestes en format: node1 node2\n");
+        return -1;
+    }
 
-aresta larestes[NAR] = {{0, 1, 0},
-                        {0, 2, 0},
-                        {0, 2, 0},
-                        {0, 3, 0},
-                        {0, 3, 0},
-                        {1, 2, 0},
-                        {1, 3, 0}};
+    unsigned num_nodes, num_arestes;
+    FILE *fitxer = fopen(argv[1], "r");
+    fscanf(fitxer, "%u", &num_nodes);
+    fscanf(fitxer, "%u", &num_arestes);
 
-int main() {
-    unsigned i, arestespos[MAXARNO], npos = 0, seguir = 0, arestaposible = 0;
-    int valencies[4];
+    aresta larestes[num_arestes];
     estructuracami cami;
+    cami.nodes = (unsigned *) malloc((num_arestes + 1) * sizeof(unsigned));
+    cami.arvis = (unsigned *) malloc(num_arestes * sizeof(unsigned));
+
+    if (cami.nodes == NULL || cami.arvis == NULL) {
+        printf("No s'ha pogut reservar memòria per a l'estructura de camí.");
+        return -1;
+    }
+
+    if (llegir_arestes(fitxer, larestes, num_arestes) != num_arestes) {
+        printf("El número d'arestes llegides del fitxer no correspón al número d'arestes de la segona línia del fitxer.");
+        return -1;
+    }
+
+    unsigned max_arestesXnode = max_arestes(num_nodes, larestes, num_arestes);
+    unsigned arestespos[max_arestesXnode];
 
     cami.naresvisit = 0;
-    for (i = 0; i < NAR; i++) {
+    for (int i = 0; i < num_arestes; i++) {
         printf("Aresta %d -> [%d,%d]\n", i, larestes[i].n1, larestes[i].n2);
     }
-    printf("Les valencies dels nodes son:\n");
-    calculavalencies(4, valencies);
-    for (i = 0; i < 4; i++) printf("El node %u té: %u camins\n", i, valencies[i]);
+
+    unsigned valencies[num_nodes];
+    unsigned node_max_valencia = calcular_valencies(larestes, num_arestes, valencies, num_nodes);
+    printf("\nLes valencies dels nodes són:\n");
+    for (unsigned i = 0; i < 4; i++) {
+        printf("El node %u té: %u arestes\n", i, valencies[i]);
+    }
+    printf("\nEl node amb més arestes és el %u\n", node_max_valencia);
 
     printf("Node Inici: ");
-    cami.nodes[0] = -1;
     scanf("%u", cami.nodes);
-    npos = arestesdelnode(NULL, 0, cami.nodes[0], arestespos);
-    seguir = 1;
+    unsigned npos = arestesdelnode(larestes, num_arestes, cami.nodes[0], arestespos);
+    bool seguir = true;
     do {
         if (npos > 0) {
             printf("Teniu %d arestes per triar: \n", npos);
-            for (i = 0; i < npos; i++) {
-                printf("%d -> [%d,%d]\n", arestespos[i], larestes[arestespos[i]].n1,
-                       larestes[arestespos[i]].n2);
+            for (int i = 0; i < npos; i++) {
+                printf("%d -> [%d,%d]\n", arestespos[i], larestes[arestespos[i]].n1, larestes[arestespos[i]].n2);
             }
+
+            bool arestaposible = false;
             do {
                 printf("Trieu: ");
                 scanf("%u", &cami.arvis[cami.naresvisit]);
-                arestaposible = 0;
-                for (i = 0; i < npos; i++) {
-                    if (arestespos[i] == cami.arvis[cami.naresvisit]) arestaposible = 1;
+                for (int i = 0; i < npos; i++) {
+                    if (arestespos[i] == cami.arvis[cami.naresvisit]) {
+                        arestaposible = true;
+                    }
                 }
-            } while (arestaposible == 0);
+            } while (!arestaposible);
+
             larestes[cami.arvis[(cami.naresvisit)]].estat = 1;
             (cami.naresvisit)++;
             if (larestes[cami.arvis[(cami.naresvisit) - 1]].n1 == cami.nodes[(cami.naresvisit) - 1]) {
@@ -76,29 +102,61 @@ int main() {
             } else {
                 cami.nodes[cami.naresvisit] = larestes[cami.arvis[(cami.naresvisit) - 1]].n1;
             }
-            for (i = 0; i < cami.naresvisit; i++) {
+            for (int i = 0; i < cami.naresvisit; i++) {
                 printf("%d [%d,%d] ", cami.nodes[i],
                        larestes[cami.arvis[i]].n1, larestes[cami.arvis[i]].n2);
             }
             printf("%d\n", cami.nodes[cami.naresvisit]);
-            npos = arestesdelnode(NULL, 0, cami.nodes[cami.naresvisit], arestespos);
+            npos = arestesdelnode(larestes, num_arestes, cami.nodes[cami.naresvisit], arestespos);
         } else {
             printf("No queden arestes disponibles.\n");
             printf("Arestes visitades (%d):\n", cami.naresvisit);
-            for (i = 0; i < cami.naresvisit; i++) {
+            for (int i = 0; i < cami.naresvisit; i++) {
                 printf("%d ", cami.arvis[i]);
             }
-            seguir = 0;
+            seguir = false;
         }
-
     } while (seguir);
     printf("\n");
     return 0;
 }
 
+unsigned llegir_arestes(FILE *fitxer, aresta larestes[], unsigned num_arestes) {
+    unsigned i = 0, n1, n2;
+    while ((fscanf(fitxer, "%u %u", &n1, &n2)) != EOF) {
+        if (i < num_arestes) {
+            larestes[i].n1 = n1;
+            larestes[i].n2 = n2;
+            larestes[i].estat = 0;
+        }
+        ++i;
+    }
+    return i;
+}
+
+unsigned max_arestes(unsigned num_nodes, aresta larestes[], unsigned num_arestes) {
+    unsigned arestesXnode[num_nodes], max = 0;
+    for (int i = 0; i < num_nodes; ++i) {
+        arestesXnode[i] = 0;
+    }
+
+    for (int i = 0; i < num_arestes; ++i) {
+        ++arestesXnode[larestes[i].n1];
+        ++arestesXnode[larestes[i].n2];
+    }
+
+    for (int i = 0; i < num_nodes; ++i) {
+        if (arestesXnode[i] > max) {
+            max = arestesXnode[i];
+        }
+    }
+
+    return max;
+}
+
 unsigned arestesdelnode(aresta larestes[], unsigned num_arestes, unsigned nn, unsigned int llistap[]) {
     unsigned i, nombre = 0;
-    for (i = 0; i < NAR; i++) {
+    for (i = 0; i < num_arestes; i++) {
         if (larestes[i].estat == 0 && (larestes[i].n1 == nn || larestes[i].n2 == nn)) {
             llistap[nombre] = i;
             nombre++;
@@ -107,19 +165,23 @@ unsigned arestesdelnode(aresta larestes[], unsigned num_arestes, unsigned nn, un
     return nombre;
 }
 
-int calculavalencies(unsigned nnodes, unsigned val[]) {
-    unsigned i;
-    for (i = 0; i < nnodes; i++) {
-        val[i] = 0;
-    }
-    for (i = 0; i < NAR; i++) {
-        val[larestes[i].n1]++;
-        val[larestes[i].n2]++;
 
+unsigned int calcular_valencies(aresta *larestes, unsigned num_arestes, unsigned *valencies, unsigned num_nodes) {
+    for (unsigned i = 0; i < num_nodes; i++) {
+        valencies[i] = 0;
     }
-    unsigned max = 0;
-    for (i = 0; i < nnodes; i++) {
-        if (val[i] > max) max = val[i];
+
+    for (unsigned i = 0; i < num_arestes; i++) {
+        valencies[larestes[i].n1]++;
+        valencies[larestes[i].n2]++;
     }
-    return max;
+
+    unsigned node_max_valencia = 0;
+    for (unsigned i = 0; i < num_nodes; i++) {
+        if (valencies[i] > valencies[node_max_valencia]) {
+            node_max_valencia = i;
+        }
+    }
+
+    return node_max_valencia;
 }
